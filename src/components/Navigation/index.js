8ec872bdada3ece16d8f49895353fb0f830e4b31
christopherstrolia-davis <chris.strolia-davis@swarmsolutions.com>
2022-08-21 09:42:57 -0400

    BUGFIX - csd - Added toggle menu functionality for mobile view size.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,22 +1,26 @@
 import { firstCap } from "../../utils/string-helpers";
+import { useState } from "react";
 
 export default function Navigation({ currentSection, setCurrentSection, sections }) {
+  const [ menuOpen, setMenuOpen ] = useState(false);
   const vm = {
+    menuToggle: () => setMenuOpen(!menuOpen),
+    menuVisible: menuOpen ? 'menu-visible' : '',
     menuItems: sections.map((section) => ({
       ...section,
-      ref: 'about' === section.name ? '#' : `#${firstCap(section.name)}`,
+      ref: 'about'===section.name ? '#':`#${ firstCap(section.name) }`,
       key: section.name,
       onClick: () => {
         setCurrentSection(section)
       },
-      selected: currentSection.name === section.name ? 'selected' : null,
+      selected: currentSection.name===section.name ? 'selected':null,
     })),
   }
 
   return (
-    <nav className="nav-bar">
+    <nav className={ `nav-bar ${ vm.menuVisible }` }>
       <div className="portfolio-owner">Chris Strolia-Davis</div>
-      <div className="view-nav-toggle">
+      <div className="view-nav-toggle" onClick={ vm.menuToggle }>
         <span className="line"></span>
         <span className="line"></span>
         <span className="line"></span>
@@ -25,8 +29,8 @@ export default function Navigation({ currentSection, setCurrentSection, sections
         <ul>
           {
             vm.menuItems.map((item) => (
-              <li className={item.selected} key={item.key}>
-                <a href={ item.ref } onClick={item.onClick}>{ item.title }</a>
+              <li className={ item.selected } key={ item.key }>
+                <a href={ item.ref } onClick={ item.onClick }>{ item.title }</a>
               </li>
             ))
           }
